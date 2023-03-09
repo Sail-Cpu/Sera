1381daade3888ec93ea2cb2f276e844667aebcb0
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-03-09 14:49:59 +0100

    Implement Image Delete

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
 const bodyParser = require('body-parser');
 const images = require('./routers/ImageRouter');
 
--- routers/ImageRouter.js
+++ routers/ImageRouter.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-const levenshtein = require('fast-levenshtein');
 
 //GETTORS
 
 function Parameter(images, type, search, page, pageSize){
     if(type) images = images.filter(image => image.type === type);
-    if(search) images = images.filter(image => levenshtein.get(search, image.name) <= 10);
+    if(search) images = images.filter(image => image.name.includes(search));
     if(!page && pageSize) images = images.slice(0, pageSize);
     if(page && !pageSize) images = images.slice(page * 20, page * 20 + 20);
     if(page && pageSize) images = images.slice(page * pageSize, page * pageSize + pageSize);
@@ -17,12 +16,13 @@ function Parameter(images, type, search, page, pageSize){
 //Get all images
 router.get(`/images`, async (req, res) => {
     try{
+        let { type, search, page, page_size } = req.query;
         let query = 'select * from image;';
         pool.query(query, (err, result) => {
             if(err) throw err;
             let images = result.rows;
-            images = Parameter(images, req.query.type, req.query.search, parseInt(req.query.page), parseInt(req.query.page_size));
-            res.json(images);
+            images = Parameter(images, type, search, parseInt(page), parseInt(page_size));
+            res.send({data: images});
         })
     }catch(err){
         console.log(err);
@@ -36,7 +36,7 @@ router.get(`/images/:imageId`, async (req, res) => {
         const query = 'select * from image where id=$1';
         pool.query(query, [imageId], (err, result) => {
             if(err) throw err;
-            res.json(result.rows);
+            res.send({data: result.rows});
         })
     }catch(err){
         console.log(err);
@@ -50,10 +50,10 @@ router.post("/images", async (req, res) => {
         const query = 'select * from image where name=$1 or link=$2';
         pool.query(query, [name, link], async (err, result) => {
             if(err) throw err;
-            if(result.rowCount == 0){
+            if(result.rowCount === 0){
                 if(name.length > 0 && link.length > 0){
-                    if(type == 'background' || type == 'poster' || type == 'logo'){
-                        const insertImage = await pool.query(`insert into image(name, link, type) values($1, $2, $3)`, [name, link, type], (err, result) => {
+                    if(type === 'background' || type === 'poster' || type === 'logo'){
+                        await pool.query(`insert into image(name, link, type) values($1, $2, $3)`, [name, link, type], (err, result) => {
                             if (err) throw err;
                             res.send({data: result.rows, message:'image added successfully.'});
                         })
@@ -70,6 +70,19 @@ router.post("/images", async (req, res) => {
         
     }catch(error){
         console.log(error)
+    }
+})
+
+router.delete(`/images/:imageId`, async (req, res) => {
+    try{
+        let { imageId } = req.params;
+        let update = `delete from image where id=$1 returning *`;
+        pool.query(update, [imageId], async (error, result) => {
+            if (error) throw error;
+            res.send({data: result.rows, message: "The image has been successfully deleted"});
+        });
+    }catch (error){
+        console.log(error);
     }
 })
 
